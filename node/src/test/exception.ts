333182262a169c69597d17279c644e802a6890bd
Dan Ruan <danrsaquetti@gmail.com>
2023-07-30 16:19:57 -0300

    ref: .

--- node/src/test/exception.ts
+++ node/src/test/exception.ts
@@ -4,15 +4,25 @@ const error = new ResultException({
     message: 'Error example'
 })
 
-console.log(error)
+// console.log(error)
 
-console.log("-------------------------")
-console.log(error.getError())
-console.log(error.getDescription())
-console.log(error.getMessage())
-console.log(error.getStack())
-console.log(error.getStatus())
-console.log(error.getTitle())
-console.log("-------------------------")
+// console.log("-------------------------")
+// console.log(error.getError())
+// console.log(error.getDescription())
+// console.log(error.getMessage())
+// console.log(error.getStack())
+// console.log(error.getStatus())
+// console.log(error.getTitle())
+// console.log("-------------------------")
 
-throw error
+try {
+    throw error
+} catch (e: any) {
+    if (e instanceof Error) {
+        console.log(e)
+    }
+
+    console.log("-----------")
+
+    throw e
+}
