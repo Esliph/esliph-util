4f44e206c8ed4059e365e5922fd97fc9b45474d1
Dan Ruan <101991292+Danrley-Ruan-Saquetti@users.noreply.github.com>
2023-07-31 17:48:51 +0000

    update excepttion extends Error -> Result

--- node/src/lib/exception/index.ts
+++ node/src/lib/exception/index.ts
@@ -1,40 +1,58 @@
 import { ErrorResult } from '../error'
+import { ResultModel } from '../result'
 
-export interface ResultExceptionModel extends ErrorResult {
-    status?: number
-}
+export type ResultExceptionModel<T = any> = ResultModel<T>
 
 export interface ResultExceptionArgs {
     title?: string
     message: string
     description?: string
     status?: number
-    causes?: { message: string, origin?: string }[]
+    causes?: { message: string; origin?: string }[]
 }
 
-export class ResultException extends Error implements ResultExceptionModel {
+export class ResultException<T = any> extends Error implements ResultExceptionModel<T> {
     readonly message: string
-    readonly status?: number
+    readonly ok: boolean
     readonly stack?: string
-    readonly title: string
-    readonly description?: string
-    readonly causes: { message: string; origin?: string }[]
+    readonly status: number
+    readonly value: any
+    readonly error: ErrorResult
 
-    constructor({ message, description, title, status, causes = [] }: ResultExceptionArgs) {
+    constructor({ message, description, title, status = 0, causes = [] }: ResultExceptionArgs) {
         super()
 
         this.message = message
-        this.description = description
-        this.title = title || this.constructor.name.match(/[A-Z][a-z]+|[0-9]+/g)?.join(' ') || 'Error'
         this.status = status
-        this.causes = causes
+        this.ok = false
+        this.value = null
+        this.error = {
+            causes,
+            message,
+            title: title || this.constructor.name.match(/[A-Z][a-z]+|[0-9]+/g)?.join(' ') || 'Error',
+            description,
+        }
     }
 
-    getError() { return { message: this.message, title: this.title, description: this.description, status: this.status, causes: this.causes } }
-    getTitle() { return this.title }
-    getMessage() { return this.message }
-    getDescription() { return this.description || null }
-    getStatus() { return this.status }
-    getStack() { return this.stack || null }
-    getCauses() { return this.causes }
+    getError() {
+        return { ...this.error, status: this.status }
+    }
+    getTitle() {
+        return this.error.title
+    }
+    getMessage() {
+        return this.error.message
+    }
+    getDescription() {
+        return this.error.description || null
+    }
+    getStatus() {
+        return this.status
+    }
+    getStack() {
+        return this.stack || null
+    }
+    getCauses() {
+        return this.error.causes
+    }
 }
