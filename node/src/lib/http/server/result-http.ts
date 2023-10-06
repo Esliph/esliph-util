2b778f4626b0b08b3a9b563a7c6b17dbc6435215
Dan Ruan <101991292+Danrley-Ruan-Saquetti@users.noreply.github.com>
2023-10-06 13:03:11 +0000

    feat(node): add result http in server

---
+++ node/src/lib/http/server/result-http.ts
@@ -0,0 +1,56 @@
+import { ErrorResult, ErrorResultInfo } from '../../error'
+import { ResultModel, ResultModelBase } from '../../result'
+
+export type ResultHttpModel<ResultValueModel = any> = { dateTime: Date; timeResponse: number } & ResultModel<ResultValueModel>
+
+export class ResultHttp<ResultValueModel = any> extends ResultModelBase<ResultValueModel> {
+    protected dateTime: Date
+    protected timeResponse: number
+
+    constructor({ error, ok, status, value, dateTime, timeResponse }: ResultHttpModel<ResultValueModel>) {
+        super({ error, ok, status, value })
+
+        this.dateTime = dateTime
+        this.timeResponse = timeResponse
+    }
+
+    static success<ResultValueModel = any>(successInfo: ResultValueModel, times?: { dateTime?: Date; timeResponse?: number }, statusCode = 200) {
+        const timesM = { dateTime: new Date(Date.now()), timeResponse: 0, ...times }
+        return new ResultHttp<ResultValueModel>({
+            ok: true,
+            status: statusCode,
+            value: successInfo,
+            error: null,
+            dateTime: timesM.dateTime,
+            timeResponse: timesM.timeResponse,
+        })
+    }
+
+    static failure<ResultValueModel = any>(errorInfo: ErrorResultInfo, times?: { dateTime?: Date; timeResponse?: number }, statusCode = 400) {
+        const timesM = { dateTime: new Date(Date.now()), timeResponse: 0, ...times }
+        return new ResultHttp<ResultValueModel>({
+            ok: false,
+            status: statusCode,
+            error: new ErrorResult(errorInfo),
+            value: null,
+            dateTime: timesM.dateTime,
+            timeResponse: timesM.timeResponse,
+        })
+    }
+
+    static inherit<ResultValueModelInherited = any>({ ok, status, value, error, dateTime, timeResponse }: ResultHttpModel<ResultValueModelInherited>) {
+        return new ResultHttp<ResultValueModelInherited>({ ok, status, error, value, dateTime, timeResponse })
+    }
+
+    getResponse() {
+        return { ...super.getResponse(), dateTime: this.dateTime }
+    }
+
+    getDateTime() {
+        return this.dateTime
+    }
+
+    getTimeResponse() {
+        return this.timeResponse
+    }
+}
